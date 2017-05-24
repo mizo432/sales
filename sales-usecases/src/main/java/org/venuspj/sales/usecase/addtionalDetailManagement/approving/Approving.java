c4e6f6ca16d7584d33ce72e3cb63ff0e25d249b9
Mizoguchi <kenichi.mizoguchi@asoview.co.jp>
2017-05-24 10:17:43 +0900

    useCaseモジュールを追加した

---
+++ sales-usecases/src/main/java/org/venuspj/sales/usecase/addtionalDetailManagement/approving/Approving.java
@@ -0,0 +1,8 @@
+package org.venuspj.sales.usecase.addtionalDetailManagement.approving;
+
+import org.springframework.stereotype.Service;
+import org.venuspj.sales.core.usecase.addtionalDetailManagement.approvingAdditionDetail.ApprovingAdditionDetail;
+
+@Service
+public class Approving implements ApprovingAdditionDetail {
+}
