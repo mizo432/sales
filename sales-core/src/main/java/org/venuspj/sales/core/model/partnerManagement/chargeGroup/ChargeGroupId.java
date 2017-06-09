88d70010f439c76277e470a09f84afeace53efc4
Mizoguchi <kenichi.mizoguchi@asoview.co.jp>
2017-06-09 14:26:53 +0900

    Findbugsに対応

--- sales-core/src/main/java/org/venuspj/sales/core/model/partnerManagement/chargeGroup/ChargeGroupId.java
+++ sales-core/src/main/java/org/venuspj/sales/core/model/partnerManagement/chargeGroup/ChargeGroupId.java
@@ -1,11 +1,21 @@
 package org.venuspj.sales.core.model.partnerManagement.chargeGroup;
 
+import org.venuspj.sales.utils.Objects2;
+
 public class ChargeGroupId {
     private Integer value;
 
     public ChargeGroupId(Integer aValue) {
         value = aValue;
 
+    }
+
+    @Override
+    public String toString() {
+        return Objects2
+                .toStringHelper(this)
+                .addValue(value)
+                .toString();
     }
 
 }
---
+++ sales-usecases/src/main/java/org/venuspj/sales/usecase/reconciliatingManagement/importingBankAccountLog/ImportingBankAccountLogImpl.java
@@ -0,0 +1,12 @@
+package org.venuspj.sales.usecase.reconciliatingManagement.importingBankAccountLog;
+
+import org.venuspj.sales.core.model.bankBook.log.BankAccountLog;
+import org.venuspj.sales.core.usecase.reconciliatingManagement.importingBankAccountLog.ImportingBankAccountLog;
+
+public class ImportingBankAccountLogImpl implements ImportingBankAccountLog {
+
+    @Override
+    public void doIt(BankAccountLog aBankAccountLog) {
+
+    }
+}
