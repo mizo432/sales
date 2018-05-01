c5d5511e66a7ea904b18f3006a2a248c749207ca
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2018-05-01 16:13:47 +0900

    findBugsに対応した Number

--- models/core/src/main/java/org/venuspj/sales/core/fundamentals/number/Number.java
+++ models/core/src/main/java/org/venuspj/sales/core/fundamentals/number/Number.java
@@ -1,5 +1,7 @@
 package org.venuspj.sales.core.fundamentals.number;
 
+import static org.venuspj.sales.utils.Objects2.equal;
+import static org.venuspj.sales.utils.Objects2.hash;
 import static org.venuspj.sales.utils.Objects2.isNull;
 
 public class Number implements Comparable<Number> {
@@ -13,5 +15,16 @@ public class Number implements Comparable<Number> {
     public int compareTo(Number o) {
         if (isNull(o)) return -1;
         return value.compareTo(o.value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (isNull(obj)) return false;
+        return equal(this.value, ((Number) obj).value);
+    }
+
+    @Override
+    public int hashCode() {
+        return hash(value);
     }
 }
