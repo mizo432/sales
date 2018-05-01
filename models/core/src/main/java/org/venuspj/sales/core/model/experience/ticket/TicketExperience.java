8408938b7b82f9487eb7ddc7f1273cdc3bdbb39a
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2018-05-01 12:40:02 +0900

    リファクタリング対応もれ

--- models/core/src/main/java/org/venuspj/sales/core/model/experience/ticket/TicketExperience.java
+++ models/core/src/main/java/org/venuspj/sales/core/model/experience/ticket/TicketExperience.java
@@ -1,6 +1,8 @@
 package org.venuspj.sales.core.model.experience.ticket;
 
 import org.venuspj.sales.core.model.experience.AbstractExperience;
+import org.venuspj.sales.core.model.experience.ApplicationDate;
+import org.venuspj.sales.core.model.experience.ExperienceDate;
 import org.venuspj.sales.core.model.partnerManagement.chargeGroup.ChargeGroupId;
 
 public class TicketExperience extends AbstractExperience {
@@ -8,7 +10,9 @@ public class TicketExperience extends AbstractExperience {
 
     }
 
-    public TicketExperience(ChargeGroupId chargeGroupId) {
-        super(chargeGroupId);
+    public TicketExperience(ChargeGroupId chargeGroupId,
+                            ApplicationDate applicationDate,
+                            ExperienceDate experienceDate) {
+        super(chargeGroupId,applicationDate,experienceDate);
     }
 }
