a0a9e6ae8f6e8d4c7a1e0a0a3fe278795b643703
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2018-06-04 14:19:10 +0900

    findbugsに対応した

--- models/core/src/main/java/org/venuspj/sales/core/model/partnerManagement/customer/CustomerIdentifier.java
+++ models/core/src/main/java/org/venuspj/sales/core/model/partnerManagement/customer/CustomerIdentifier.java
@@ -5,6 +5,7 @@ import org.venuspj.ddd.model.entity.EntityIdentifier;
 import org.venuspj.sales.core.fundamentals.values.LongValue;
 
 import static org.venuspj.util.objects2.Objects2.equal;
+import static org.venuspj.util.objects2.Objects2.hash;
 import static org.venuspj.util.objects2.Objects2.nonNull;
 
 public class CustomerIdentifier extends AbstractEntityIdentifier<Customer> implements LongValue {
@@ -33,6 +34,12 @@ public class CustomerIdentifier extends AbstractEntityIdentifier<Customer> imple
     @Override
     public boolean sameValueAs(EntityIdentifier<Customer> other) {
         return other instanceof LongValue && equal(this.value, ((LongValue) other).asLong());
+
+    }
+
+    @Override
+    public int hashCode() {
+        return hash(value);
 
     }
 
