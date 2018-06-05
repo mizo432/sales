6425185ee4ef7230aaebca2382e40644f1a1e6d0
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2018-06-05 15:56:14 +0900

    findbugsに対応した

--- models/core/src/main/java/org/venuspj/sales/core/model/sale/SaleDate.java
+++ models/core/src/main/java/org/venuspj/sales/core/model/sale/SaleDate.java
@@ -6,6 +6,9 @@ import org.venuspj.sales.core.fundamentals.recordDatetime.RecordDateTimeValue;
 
 import java.time.LocalDateTime;
 
+import static org.venuspj.util.objects2.Objects2.equal;
+import static org.venuspj.util.objects2.Objects2.hash;
+
 public class SaleDate implements RecordDateTimeValue {
     private LocalDateTime value;
 
@@ -34,4 +37,18 @@ public class SaleDate implements RecordDateTimeValue {
         return new SaleDate(value);
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof SaleDate && sameValueAs((SaleDate) obj);
+    }
+
+    private boolean sameValueAs(SaleDate obj) {
+        return equal(value,obj.value);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return hash(value);
+    }
 }
