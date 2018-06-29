bc06c53d5ab8478f42e664ece814afb03cd2762d
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2018-06-29 16:00:25 +0900

    chenge SalesDeposit

--- models/core/src/main/java/org/venuspj/sales/core/model/sale/SalesDeposit.java
+++ models/core/src/main/java/org/venuspj/sales/core/model/sale/SalesDeposit.java
@@ -1,4 +1,19 @@
 package org.venuspj.sales.core.model.sale;
 
-public class SalesDeposit {
+import org.venuspj.sales.core.fundamentals.amount.AbstractAmountValue;
+
+public class SalesDeposit extends AbstractAmountValue<SalesDeposit> {
+    public SalesDeposit() {
+        super();
+
+    }
+
+    SalesDeposit(Long value) {
+        super(value);
+
+    }
+
+    public static SalesDeposit yeans(Long value) {
+        return new SalesDeposit(value);
+    }
 }
